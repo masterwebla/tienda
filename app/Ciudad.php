936c8f766ba93e75bdb6a16fb0fcae1e3bbbed9e
Mauricio Mac <mac@masterweb.la>
2019-02-14 11:04:35 -0500

    Template admin integrada

--- app/Ciudad.php
+++ app/Ciudad.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Ciudad extends Model
 {
     protected $table = 'ciudades';
-    protected $fillable = ['nombre','ciudad_id'];
+    protected $fillable = ['nombre','departamento_id'];
     public $timestamps = false;
 }
